3e46158a0fba2d4b8c7fb5a9c56bb33d7fcb2bcf
Victor Cruz <VicttorrHugo100@gmail.com>
2023-03-31 14:45:04 -0300

    Add modal render function
    This function renders a modal when needed.

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+var posts = [
+    {
+        postId: 1,
+        postTitle: "Nature",
+        postDescription: "lorem ipsum de",
+        postImage: "https://cdn.corporate.walmart.com/dims4/WMT/f7a8313/2147483647/strip/true/crop/1920x1080+0+0/resize/1200x675!/quality/90/?url=https%3A%2F%2Fcdn.corporate.walmart.com%2F7b%2F66%2F142c151b4cd3a19c13e1ca65f193%2Fbusinessfornature-banner.png",
+    },
+    {
+        postId: 2,
+        postTitle: "Why Sustainable Fashion is the Future",
+        postDescription: "Learn about the environmental and social impact of fast fashion, and why sustainable fashion is the way forward.",
+        postImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3ExYAkW9JH4QBYSGIMxlofdJkyC5B0VIk5aoosXTv1QpLcNKcy6Njdv9RM1WvK5HntM4&usqp=CAU",
+    },
+    {
+        postId: 3,
+        postTitle: "The Benefits of Meditation: How it Can Improve Your Life",
+        postDescription: "Discover the physical and mental health benefits of meditation, and how it can help you manage stress and improve your overall well-being.",
+        postImage: "https://naturecanada.ca/wp-content/uploads/2022/06/NBCS-municipality_06-1024x1024.jpg",
+    },
+];
 var comments = [
     {
         id: 1,
@@ -74,6 +94,37 @@ modalContent === null || modalContent === void 0 ? void 0 : modalContent.addEven
     event.stopPropagation();
 });
 document.addEventListener("click", closeModal);
+function createModal(post) {
+    var postHolder = document.querySelector("#modalContentId");
+    if (postHolder) {
+        //Create elements
+        var section = document.createElement("section");
+        var article = document.createElement("article");
+        var h1 = document.createElement("h1");
+        var p = document.createElement("p");
+        var figure = document.createElement("figure");
+        var img = document.createElement("img");
+        var commentsHolder = document.createElement("ul");
+        commentsHolder.id = "commentContentHolder";
+        //Placing elements inside others
+        postHolder.appendChild(section);
+        postHolder.appendChild(figure);
+        section.appendChild(article);
+        article.appendChild(h1);
+        article.appendChild(p);
+        figure.appendChild(img);
+        section.appendChild(commentsHolder);
+        //Giving class to elements
+        figure.classList.add("left");
+        article.classList.add("postDescription");
+        section.classList.add("right");
+        img.classList.add("postImg");
+        commentsHolder.classList.add("comments");
+        h1.textContent = post.postTitle;
+        img.src = post.postImage;
+        p.textContent = post.postDescription;
+    }
+}
 function createComments(comments) {
     var commentHolder = document.querySelector("#commentContentHolder");
     if (commentHolder) {
@@ -92,5 +143,5 @@ function createComments(comments) {
         });
     }
 }
-;
+createModal(posts[0]);
 createComments(comments);
--- app.ts
+++ app.ts
@@ -1,3 +1,36 @@
+type Post = {
+  postId: number;
+  postTitle: string;
+  postDescription: string;
+  postImage: string;
+};
+
+const posts: Post[] = [
+  {
+    postId: 1,
+    postTitle: "Nature",
+    postDescription: "lorem ipsum de",
+    postImage:
+      "https://cdn.corporate.walmart.com/dims4/WMT/f7a8313/2147483647/strip/true/crop/1920x1080+0+0/resize/1200x675!/quality/90/?url=https%3A%2F%2Fcdn.corporate.walmart.com%2F7b%2F66%2F142c151b4cd3a19c13e1ca65f193%2Fbusinessfornature-banner.png",
+  },
+
+  {
+    postId: 2,
+    postTitle: "Why Sustainable Fashion is the Future",
+    postDescription:
+      "Learn about the environmental and social impact of fast fashion, and why sustainable fashion is the way forward.",
+    postImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3ExYAkW9JH4QBYSGIMxlofdJkyC5B0VIk5aoosXTv1QpLcNKcy6Njdv9RM1WvK5HntM4&usqp=CAU",
+  },
+
+  {
+    postId: 3,
+    postTitle: "The Benefits of Meditation: How it Can Improve Your Life",
+    postDescription:
+      "Discover the physical and mental health benefits of meditation, and how it can help you manage stress and improve your overall well-being.",
+    postImage: "https://naturecanada.ca/wp-content/uploads/2022/06/NBCS-municipality_06-1024x1024.jpg",
+  },
+];
+
 type PostComment = {
   id: number;
   postId: number;
@@ -71,6 +104,8 @@ const comments: PostComment[] = [
 const modal = document.querySelector<HTMLDivElement>("#modal");
 const modalContent = document.querySelector<HTMLDivElement>("#modalContentId");
 
+
+
 function openModal() {
   if (modal) modal.style.display = "flex";
 }
@@ -84,6 +119,48 @@ modalContent?.addEventListener("click", (event) => {
 });
 
 document.addEventListener("click", closeModal);
+
+function createModal(post: Post) {
+  const postHolder= document.querySelector<HTMLDialogElement>("#modalContentId");
+  if(postHolder){
+  
+   //Create elements
+   
+    const section = document.createElement("section");
+    const article = document.createElement("article");
+    const h1 = document.createElement("h1");
+    const p = document.createElement("p");
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const commentsHolder = document.createElement("ul");
+    commentsHolder.id="commentContentHolder"
+    //Placing elements inside others
+    
+  
+    postHolder.appendChild(section);
+    postHolder.appendChild(figure);
+    section.appendChild(article);
+    article.appendChild(h1);
+    article.appendChild(p); 
+    figure.appendChild(img);
+    section.appendChild(commentsHolder);
+    
+    //Giving class to elements
+    
+    
+    figure.classList.add("left");
+    article.classList.add("postDescription");
+    section.classList.add("right");
+    img.classList.add("postImg");
+    commentsHolder.classList.add("comments");
+
+   
+    h1.textContent= post.postTitle;
+    img.src= post.postImage;
+    p.textContent= post.postDescription;
+}
+  
+}
 
 function createComments(comments: PostComment[]) {
   const commentHolder = document.querySelector<HTMLUListElement>(
@@ -104,7 +181,9 @@ function createComments(comments: PostComment[]) {
       commentHolder.appendChild(li);
     });
   }
-  
-};
+}
 
+
+
+createModal(posts[0]);
 createComments(comments);
